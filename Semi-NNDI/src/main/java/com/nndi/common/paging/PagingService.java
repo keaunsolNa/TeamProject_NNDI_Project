e3164f8f8ce7cd7ce8e0481a7cddfa43d246c0e8
keaunsolNa <knsol1992@naver.com>
2022-07-30 22:06:52 +0900

    22.07.30 푸쉬 전 수정 사항 커밋

--- Semi-NNDI/src/main/java/com/nndi/common/paging/PagingService.java
+++ Semi-NNDI/src/main/java/com/nndi/common/paging/PagingService.java
@@ -22,39 +22,16 @@ public class PagingService {
 	/* 강좌 전체 게시글 수 조회 */
 	public int selectClassTotalCount(Map<String, String> searchMap) {
 		
-		System.out.println("전체 게시글 수 조회하는 Service 도착 확인");
 		SqlSession sqlSession = getSqlSession();
 		
 		mapper =  sqlSession.getMapper(PagingMapper.class);
 		
 		int totalCount = mapper.selectClassTotalCount(searchMap);
 		
-		System.out.println("Service에서 조회한 Class의 전체 게시글 수 조회: " + totalCount);
-		
 		sqlSession.close();
 		
 		return totalCount;
 	}
-
-	/* 강좌 전체 조회 */
-//	public List<ClassesAndTeacherAndCenterDTO> selectBoradList(PagingDTO pagingDTO) {
-//		System.out.println("강좌 전체 조회용 Service 도착 확인");
-//		SqlSession sqlSession = getSqlSession();
-//		
-//		System.out.println("Servlet에서 전달받은 pagingDTO 값 확인: " + pagingDTO);
-//		
-//		mapper =  sqlSession.getMapper(PagingMapper.class);
-//		
-//		List<ClassesAndTeacherAndCenterDTO> classboardList = mapper.selectClassList(pagingDTO);
-//		
-//		
-//		System.out.println("Service에서 조회한 Class의 전체 게시글 : " + classboardList);
-//		
-//		sqlSession.close();
-//		
-//		return classboardList;
-//		
-//	}
 
 	public Map<String, List<ClassesAndTeacherAndCenterDTO>> ClassList(PagingDTO pagingDTO) {
 		SqlSession sqlSession = getSqlSession();
@@ -62,10 +39,8 @@ public class PagingService {
 		mapper = sqlSession.getMapper(PagingMapper.class);
 		
 		List<ClassesAndTeacherAndCenterDTO> classdto = mapper.selectClassList(pagingDTO);		//신청가능 강좌 목록
-		List<ClassesAndTeacherAndCenterDTO> classdto2 = mapper.ClassOffList(); //신청 불가 강좌 목록
+		List<ClassesAndTeacherAndCenterDTO> classdto2 = mapper.ClassOffList(); 					//신청 불가 강좌 목록
 		
-		System.out.println("dto 서비스 확인용" + classdto);
-
 		Map<String, List<ClassesAndTeacherAndCenterDTO>> classList = new HashMap<String, List<ClassesAndTeacherAndCenterDTO>>();
 		
 		classList.put("possibleCls", classdto);
